9e7d463386a30365eda1c77df643523a13cf190f
Daniel Supernault <danielsupernault@gmail.com>
2020-01-31 02:10:05 -0700

    wip

--- src/Api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Pixelfed;
-
-class Api
-{
-
-}
---
+++ src/PixelfedApi.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Pixelfed;
+
+use \Zttp\Zttp;
+
+class PixelfedApi
+{
+	protected $domain;
+	protected $client;
+	protected $curl;
+
+	public function __construct(string $domain, $accessToken = null)
+	{
+		$this->domain = $domain;
+		$this->client = !$accessToken ? Zttp::new() : Zttp::new()->withHeaders([
+			'Authorization' => "Bearer {$accessToken}"
+		]);
+	}
+
+	protected function furl(string $url)
+	{
+		$this->curl = $this->domain . $url;
+	}
+
+	public function user()
+	{
+		$this->furl("/api/v1/accounts/verify_credentials");
+		return $this->get();
+	}
+
+	public function accountById(string $id)
+	{
+		$this->furl("/api/v1/accounts/{$id}");
+		return $this->get();
+	}
+
+	public function accountFollowersById(string $id)
+	{
+		$this->furl("/api/v1/accounts/{$id}/followers");
+		return $this->get();
+	}
+
+	public function accountFollowingById(string $id)
+	{
+		$this->furl("/api/v1/accounts/{$id}/following");
+		return $this->get();
+	}
+
+	public function accountStatusesById(string $id)
+	{
+		$this->furl("/api/v1/accounts/{$id}/statuses");
+		return $this->get();
+	}
+
+	protected function get()
+	{
+		return $this->client->get($this->curl)->json();
+	}
+}
